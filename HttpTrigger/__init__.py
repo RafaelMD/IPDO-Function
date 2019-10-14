f634199b66d612b0f6e1050a4a59596ef2d3ef16
Rafael Monteiro Dourado <rafael.dourado@programmers.com.br>
2019-10-14 13:10:41 -0300

    Adicionando os outros estados

--- HttpTrigger/__init__.py
+++ HttpTrigger/__init__.py
@@ -12,19 +12,19 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
         lista = []
         lista.extend(getValuesList("sin", excel.iloc[5:11, 1:5]))
         lista.extend(getValuesList("norte", excel.iloc[17:20, 2:6], False))
+        lista.extend(getValuesList("nordeste", excel.iloc[17:21, 8:12], False))
+        lista.extend(getValuesList("sudeste", excel.iloc[26:30, 8:12], False))
+        lista.extend(getValuesList("sul", excel.iloc[35:38, 8:12], False))
         return func.HttpResponse(json.dumps(lista))
     except Exception:
-        return func.HttpResponse(
-             "Erro",
-             status_code=400
-        )
+        return func.HttpResponse("Erro", status_code=400)
 
 def getValuesList(regiao, df, inverso = True):
     lista = []
     for item in df.values:
         objeto = {
             "regiao": regiao,
-            "tipo" : item[0],
+            "tipo" : item[0].strip(),
             "programado" : item[1] if inverso else item[3],
             "verificado" : item[2],
             "desvio": item[3] if inverso else item[1]
